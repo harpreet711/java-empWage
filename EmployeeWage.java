1bad290849a47b66dd8dc577dd728cebaabc0973
Harpreet Kaur <happu711@gmail.com>
2020-10-12 07:39:46 +0530

    Added code to calculate monthly wages

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -8,6 +8,7 @@ public class EmployeeWage {
 	static int perHour = 20;
 	static int dailyWage = 0;
 	static int partTime = 8;
+	static int perMonth = 20;
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
@@ -22,13 +23,9 @@ public class EmployeeWage {
 		default:
 			break;
 		}
-		if (employeeAttendance()) {
-			System.out.println("Employee : Present");
-			getDailywage();
-			System.out.println(dailyWage);
-		} else {
-			System.out.println("Employee : Absent");
-		}		
+		getDailywage();
+		System.out.println("Daily wage: " + dailyWage);
+		System.out.println("Wages for month: " + getWagesForMonth());		
 	}
 	
 	public static boolean employeeAttendance() {
@@ -43,6 +40,20 @@ public class EmployeeWage {
 	private static String getEmployeeType() {
 		String[] arr = { "Full-time", "Part-time" };
 		return arr[new Random().nextInt(arr.length)];
+	}
+	
+	private static long getWagesForMonth() {
+		long wages = 0;
+		for (int i = 1; i <= perMonth; i++) {
+			System.out.println("Day : " + i);
+			if (employeeAttendance()) {
+				System.out.println("Present");
+				wages += dailyWage;
+			} else {
+				System.out.println("Absent");
+			}
+		}
+		return wages;
 	}
 }
 
