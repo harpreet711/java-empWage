78aeaeff7ab8227649193dec38ce20f14c016bf3
Harpreet Kaur <happu711@gmail.com>
2020-10-12 07:32:56 +0530

    Refactor code using switch case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -8,18 +8,29 @@ public class EmployeeWage {
 	static int perHour = 20;
 	static int dailyWage = 0;
 	static int partTime = 8;
-
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
+		
+		switch (getEmployeeType()) {
+		case "Full-time":
+			System.out.println("Employment: Full-time");
+			break;
+		case "Part-time":
+			System.out.println("Employment: Part-time");
+			break;
+		default:
+			break;
+		}
 		if (employeeAttendance()) {
 			System.out.println("Employee : Present");
+			getDailywage();
+			System.out.println(dailyWage);
 		} else {
 			System.out.println("Employee : Absent");
-		}
-		getDailywage();
-		System.out.println(dailyWage);
+		}		
 	}
-
+	
 	public static boolean employeeAttendance() {
 		return new Random().nextBoolean();
 	}
@@ -27,6 +38,11 @@ public class EmployeeWage {
 	public static int getDailywage() {
 		dailyWage = fullDay * perHour;
 		return dailyWage;
+	}
+	
+	private static String getEmployeeType() {
+		String[] arr = { "Full-time", "Part-time" };
+		return arr[new Random().nextInt(arr.length)];
 	}
 }
 
