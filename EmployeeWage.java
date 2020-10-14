268be97523e315734e349b11997e42cf809f83bd
Harpreet Kaur <happu711@gmail.com>
2020-10-14 08:40:26 +0530

    Refactor code to add multiple companies

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,57 +4,49 @@ import java.util.Random;
 
 public class EmployeeWage {
 	
-	static int fullDay = 8;
-	static int perHour = 20;
-	static int dailyWage = 0;
-	static int partTime = 8;
-	static int perMonth = 20;
-	static int forMonth = 100;
+	private static final int fullDay = 8;
+	private String companyName = "";
+	private static int dailyWage = 0;
+	private static int daysPerMonth = 0;
+	private static int hoursPerMonth = 0;
+	
+	public EmployeeWage(String companyName, int dailyWage, int daysPerMonth, int hoursPerMonth) {
+		// TODO Auto-generated constructor stub
+		this.companyName = companyName;
+		this.dailyWage = dailyWage;
+		this.daysPerMonth = daysPerMonth;
+		this.hoursPerMonth = hoursPerMonth;
+	}
+
+	
+	public String companyName() {
+		return companyName;
+	}	
+	
 	
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation");
-		
-		switch (employeeType()) {
-		case "Full-time":
-			System.out.println("Employment: Full-time");
-			break;
-		case "Part-time":
-			System.out.println("Employment: Part-time");
-			break;
-		default:
-			break;
-		}
-		getDailywage();
-		System.out.println("Daily wage: " + dailyWage);
-		System.out.println("Wages for month: " + wagesForMonth());		
+
+		EmployeeWage amazon = new EmployeeWage("Amazon", 100, 10, 50);
+		EmployeeWage flipkart = new EmployeeWage("Flipkart", 200, 20, 100);
+
+		System.out.println("Monthly wage for an employee of company " + amazon.companyName() + " is "
+				+ amazon.wagesForMonth());
+		System.out.println("Monthly wage for an employee of company " + flipkart.companyName() + " is "
+				+ flipkart.wagesForMonth());
 	}
 	
 	public static boolean employeeAttendance() {
 		return new Random().nextBoolean();
-	}
-
-	public static int getDailywage() {
-		dailyWage = fullDay * perHour;
-		return dailyWage;
-	}
-	
-	private static String employeeType() {
-		String[] arr = { "Full-time", "Part-time" };
-		return arr[new Random().nextInt(arr.length)];
-	}
+	}	
 	
 	private static long wagesForMonth() {
 		long wages = 0;
 		int workingHours = 0, workingDays = 0;
-		while (workingDays < perMonth && workingHours < forMonth) {
+		while (workingDays < daysPerMonth && workingHours < hoursPerMonth) {
 			workingDays++;
-			System.out.println("Day : " + workingDays);
 			if (employeeAttendance()) {
-				System.out.println("Present");
 				workingHours += fullDay;
 				wages += dailyWage;
-			} else {
-				System.out.println("Absent");
 			}
 		}
 		return wages;
